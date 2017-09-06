27357738a7812d56e02c940f582706cbf43abd54
Gary Kwong <gary@rumblingedge.com>
2017-09-06 15:22:18 -0700

    Convert "hangs" test to use argparse and update documentation.

--- src/lithium/interestingness/hangs.py
+++ src/lithium/interestingness/hangs.py
@@ -1,23 +1,44 @@
 #!/usr/bin/env python
 # coding=utf-8
-# pylint: disable=missing-docstring
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from __future__ import absolute_import, print_function
+"""Lithium's "hangs" interestingness test to assess whether a binary hangs.
+
+Example:
+    python -m lithium hangs 3 ./js --fuzzing-safe testcase.js
+"""
+
+from __future__ import absolute_import
+
+import logging
 
 from . import timed_run
 
 
-def interesting(args, temp_prefix):  # pylint: disable=missing-docstring,missing-return-doc,missing-return-type-doc
-    timeout = int(args[0])
+def interesting(cli_args, temp_prefix):
+    """Interesting if the binary causes a hang.
 
-    runinfo = timed_run.timed_run(args[1:], timeout, temp_prefix)
+    Args:
+        cli_args (list): List of input arguments.
+        temp_prefix (str): Temporary directory prefix, e.g. tmp1/1 or tmp4/1
+
+    Returns:
+        bool: True if binary causes a hang, False otherwise.
+    """
+    logger = logging.getLogger("interestingness_hangs")
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger("flake8").setLevel(logging.WARNING)
+    logger_handler = logging.StreamHandler()
+
+    timeout = int(cli_args[0])
+    runinfo = timed_run.timed_run(cli_args[1:], timeout, temp_prefix)
 
     if runinfo.sta == timed_run.TIMED_OUT:
         return True
 
-    print("Exited in %.3f seconds" % runinfo.elapsedtime)
+    logger.info("Exited in %.3f seconds", runinfo.elapsedtime)
+    logger_handler.flush()
     return False
